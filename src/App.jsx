19a5b319c6f98c16cdaf0b2dcd7fc35de70964fa
Mahmud Hasan Sizan <mahmudhasan3801@gmail.cm>
2025-05-20 00:52:17 +0600

    scroll animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import { motion } from "motion/react";
+import { motion, useScroll } from "motion/react";
 
 const App = () => {
+  
+  const {scrollYProgress} = useScroll()
+
   return (
     <div>
-      <motion.div
+      {/* <motion.div
         drag
         whileDrag={{
           scale: 1.2,
@@ -38,7 +41,70 @@ const App = () => {
         class="circle"
       >
         Framer circle
-      </motion.div>
+      </motion.div> */}
+
+      <motion.div
+      style={{ scaleX: scrollYProgress }}
+      className="bg-emerald-700 origin-left w-full fixed top-0 left-0 h-4"></motion.div>
+      <div className="text-center text-white font-mono m-8">
+        <h1 className="text-4xl mb-4 font-bold">Learning Motion</h1>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore, eius
+          asperiores, temporibus laborum in vero harum ullam ipsam omnis sunt
+          consequuntur possimus, similique molestias maxime quos aspernatur odio
+          recusandae officiis facere ipsa itaque. Sequi iste aliquam, id
+          voluptates commodi sint? Nesciunt sed officia saepe atque quia, minus
+          natus ipsum quisquam quae! Eveniet quisquam non minus consequuntur quo
+          neque possimus, illum veritatis deserunt doloribus itaque, placeat
+          nemo cupiditate natus! Quibusdam ipsum cumque sunt fugiat ad quidem
+          minima libero eaque aliquid, accusamus exercitationem quaerat,
+          praesentium consectetur at aut explicabo quas? Doloremque, iure
+          laborum? Labore esse illo excepturi vero, ipsa voluptates doloribus
+          aliquam nesciunt fugiat amet odit iure est, id necessitatibus.
+          Laboriosam iure consectetur hic sequi aperiam tempore voluptate quo
+          repellendus libero et! Deleniti iste dolorem, tenetur sint magni
+          aspernatur, in eligendi libero nihil voluptatem doloribus consectetur
+          blanditiis pariatur quibusdam doloremque ab suscipit repudiandae
+          sapiente quas qui aliquid. Nam nisi aperiam non deserunt. <br />
+          <br />
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt,
+          reiciendis voluptate officiis nam similique fugiat rerum cum, commodi
+          assumenda dolores suscipit quo ducimus sint quisquam alias. Totam
+          illum blanditiis facere quasi dolorem in dolores reiciendis quaerat
+          officiis, ipsum, magnam neque laboriosam odit, consectetur minus
+          aliquid eveniet ut dicta possimus dolorum est. Earum nobis quaerat
+          quos, totam modi quidem ipsam ipsum adipisci neque, veritatis iure
+          accusantium aperiam! Neque modi doloremque quidem molestias quas
+          nesciunt cumque, dolore esse veniam est cum, iure ipsam aperiam totam
+          magnam reprehenderit voluptates quod officia illum asperiores omnis ab
+          reiciendis hic. Minus, eaque a dolorum iure tempore laboriosam
+          perspiciatis inventore ea quibusdam nihil ex consequuntur optio?
+          Asperiores nobis molestias velit impedit recusandae tempore dolor
+          alias, amet illum suscipit ipsum eligendi, aut voluptate nemo
+          laboriosam dolore! Nisi sequi, at dignissimos autem necessitatibus
+          earum quidem voluptas possimus laudantium repellendus nihil, eos sint
+          esse aliquid repellat non vitae aspernatur molestiae. <br />
+          <br />
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit nihil
+          maiores unde tempore id. Deleniti sed, nam sunt, doloremque sit fugit
+          quisquam molestiae necessitatibus a animi iste alias culpa. Possimus
+          aspernatur cupiditate ad, nisi accusamus corrupti est consectetur
+          dolorum tenetur, atque ipsum similique animi, aperiam iste modi
+          nostrum praesentium ipsam laboriosam odit consequuntur adipisci cum
+          commodi aliquam in? Aliquid, dolor tempora. Quisquam a praesentium nam
+          consequatur, minus itaque architecto fugiat nesciunt alias voluptate
+          quo hic aliquam? Magnam quibusdam nostrum a rem expedita fugit non
+          doloremque repudiandae nemo alias aspernatur, iure enim, explicabo
+          perspiciatis architecto facilis minus? Eveniet nulla quas voluptatum
+          officiis velit minima ut dicta repellat ipsam placeat dignissimos, ex
+          iste, saepe accusamus odit molestiae reprehenderit nisi temporibus?
+          Quos aspernatur consequatur possimus deserunt recusandae incidunt ad
+          perspiciatis, aliquam totam iste rerum temporibus! Aliquam nemo iusto
+          quos maxime ea dolore non corrupti corporis voluptate. Repudiandae
+          temporibus nulla ipsam ab suscipit quo.
+        </p>
+      </div>
     </div>
   );
 };
